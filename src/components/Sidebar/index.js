5e0ee768f258a00168c1ac9983d7342af9c93144
Arif Hasanic <arif.hasanic@outlook.com>
2022-10-26 23:30:29 +0200

    add eslint with airbnb style enforcement

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,13 +1,28 @@
-import SidebarButton from '../SidebarButton';
+import React from "react";
 
-import styles from './styles.module.css';
+import SidebarButton from "../SidebarButton";
+
+import styles from "./styles.module.css";
 
 export default function Sidebar() {
-    return (
-        <div className={styles.sidebar}>
-            <SidebarButton title="Home" iconName="fa fa-home" onClick={() => alert('home')}/>
-            <SidebarButton title="Diagramm" iconName="fa fa-chart-area" onClick={() => alert('diagramm')}/>
-            <SidebarButton title="Settings" iconName="fa fa-cog" onClick={() => alert('settings')}/>
-        </div>
-    );
+  return (
+    <div className={styles.sidebar}>
+      <SidebarButton
+        title="Home"
+        iconName="fa fa-home"
+        onClick={() => alert("home")}
+      />
+      <SidebarButton
+        title="Diagramm"
+        iconName="fa fa-chart-area"
+        onClick={() => alert("diagramm")}
+      />
+
+      <SidebarButton
+        title="Settings"
+        iconName="fa fa-cog"
+        onClick={() => alert("settings")}
+      />
+    </div>
+  );
 }
