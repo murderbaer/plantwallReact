c7378a74031e50ff2a21c6d4f49840064aca871c
Arif Hasanic <arif.hasanic@outlook.com>
2022-10-28 21:34:15 +0200

    add basic boxes for layour

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import classNames from 'classnames';
 import styles from './styles.module.css';
 
 export default function Home() {
     return (
         <div className={styles.home}>
-            <h1>Home</h1>
+            <div className={classNames(styles.generalInfo, styles.box)}>
+                Title
+            </div>
+            <div className={styles.row}>
+                <div className={classNames(styles.left, styles.box)}>
+                    left
+                </div>
+                <div className={classNames(styles.right, styles.box)}>
+                    right
+                </div>
+            </div>
         </div>
     );
 }
