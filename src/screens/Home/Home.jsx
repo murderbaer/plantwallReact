bfd91f8712b1a3aebe12212b159ed4364ea9524a
Arif Hasanic <arif.hasanic@outlook.com>
2022-10-28 23:23:52 +0200

    divs inside boxes render side by side

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -5,16 +5,21 @@ import styles from './styles.module.css';
 export default function Home() {
     return (
         <div className={styles.home}>
-            <div className={classNames(styles.generalInfo, styles.box)}>
-                Title
+            <div className={classNames(styles.generalInfo, styles.box)}>Title</div>
+            <div className={styles.row}>
+                <div className={classNames(styles.left, styles.box)}>1</div>
+                <div className={classNames(styles.right, styles.box)}>
+                    <div>subside 1</div>
+                    <div>subside 2</div>
+                </div>
             </div>
             <div className={styles.row}>
                 <div className={classNames(styles.left, styles.box)}>
-                    left
+                    <div>subside 1</div>
+                    <div>subside 2</div>
+                    <div>subside 3</div>
                 </div>
-                <div className={classNames(styles.right, styles.box)}>
-                    right
-                </div>
+                <div className={classNames(styles.right, styles.box)}>right</div>
             </div>
         </div>
     );
