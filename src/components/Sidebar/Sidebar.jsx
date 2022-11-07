2dd30e49b30c993f3b20b84849ae35dbf45f6cd3
Arif Hasanic <arif.hasanic@outlook.com>
2022-11-07 13:21:44 +0100

    replace old icons

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 import SidebarButton from '../SidebarButton/SidebarButton';
 
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';import TimelineIcon from '@mui/icons-material/Timeline';import SettingsIcon from '@mui/icons-material/Settings';
 import styles from './styles.module.css';
 
 export default function Sidebar() {
     return (
         <div className={styles.sidebar}>
             <SidebarButton title="Home" Icon={DashboardIcon} path="/" />
-            <SidebarButton title="camera" Icon={DashboardIcon} path="/camera" />
-            <SidebarButton title="Diagramm" Icon={DashboardIcon} path="/diagramm" />
-            <SidebarButton title="Settings" Icon={DashboardIcon} path="/settings" />
+            <SidebarButton title="camera" Icon={CameraAltIcon} path="/camera" />
+            <SidebarButton title="Diagramm" Icon={TimelineIcon} path="/diagramm" />
+            <SidebarButton title="Settings" Icon={SettingsIcon} path="/settings" />
         </div>
     );
 }
