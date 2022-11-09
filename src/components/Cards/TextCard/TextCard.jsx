b7067b974b92003a0da780a831066f14cbda4745
Arif Hasanic <arif.hasanic@outlook.com>
2022-11-09 22:32:54 +0100

    remove dependabot

--- src/components/Cards/TextCard/TextCard.jsx
+++ src/components/Cards/TextCard/TextCard.jsx
@@ -1,17 +1,18 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './styles.module.scss';
 
-export default function TextCard({ title, text, icon, onClick }) {
+export default function TextCard({
+    title, text, icon, onClick,
+}) {
     return (
         <div className={classNames(styles.boxWrapper)} onClick={onClick}>
             <div className={classNames(styles.boxTitle)}>
                 {icon}
                 {title}
             </div>
-            <div className={styles.boxContent}>
-                {text}
-            </div>
+            <div className={styles.boxContent}>{text}</div>
         </div>
     );
 }
@@ -21,4 +22,9 @@ TextCard.propTypes = {
     text: PropTypes.string.isRequired,
     icon: PropTypes.object,
     onClick: PropTypes.func,
+};
+
+TextCard.defaultProps = {
+    icon: null,
+    onClick: () => {},
 };
